24120118022a7db4b7bb957bc35d31f7a7ef4a23
Brian Bahtiarian <brianbahtiarian@brians-mbp.myfiosgateway.com>
2020-09-20 11:53:53 -0400

    Update project

--- jleparse.py
+++ jleparse.py
@@ -3,7 +3,10 @@ from datetime import datetime, timezone
 import sys, getopt
 stop_int = -2
 free_space_int = -1
-ole_header = b'\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1'
+auto_header = b'\xd0\xcf\x11\xe0\xa1\xb1\x1a\xe1'
+custom_header = b'\x02\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x03\x00\x00\x00\x01\x14\x02\x00\x00\x00\x00\x00\xC0\x00\x00\x00\x00\x00\x00\x46'
+lnk_guid = b'\x4C\x00\x00\x00\x01\x14\x02\x00\x00\x00\x00\x00\xC0\x00\x00\x00\x00\x00\x00\x46'
+lnk_guid_size = 20
 big_endian = b'\xff\xfe'
 little_endian = b'\xfe\xff'
 msat_max_sectors = 109
@@ -200,137 +203,169 @@ def main(argv):
 	rest_of_file = jle_file.read()
 	jle_file.seek(0)
 	whole_file = jle_file.read()
-	file_sig = header[0:8]
-	#Verify file is OLE file type
-	if(file_sig != ole_header):
-		print("The specified file is not of type OLE")
-		exit()
+	if(jle_type == "a"):
+		file_sig = header[0:8]
 
-	#Get little endian or big endian
-	endian_header = header[28:30]
+		#Verify file is OLE file type
+		if(file_sig != auto_header):
+			print("The specified file is not an automatic jumplist")
+			exit()
 
-	if(endian_header == big_endian):
-		endian = ">"
+		#Get little endian or big endian
+		endian_header = header[28:30]
 
-	elif(endian_header == little_endian):
-		endian = "<"
+		if(endian_header == big_endian):
+			endian = ">"
 
-	#Get sector size
-	sector_size_header = header[30:32]
-	short_sector_size_header = header[32:34]
-	#get sector allocation table size
-	sat_size_header = header[44:48]
-	#get directory first sector SID
-	directory_first_sector_sid_header = header[48:52]
-	#get min size of standard stream
-	min_stream_size_header = header[56:60]
-	#get Sector identifier (SID) of first sector of the short-sector allocation table (SSAT).
-	short_first_sector_sid_header = header[60:64]
-	#Get Total number of sectors used for the short-sector allocation table (SSAT).
-	total_sectors_ssat_header = header[64:68]
-	#Get Sector identifier (SID) of first sector of the extra master sector allocation table (MSAT)
-	extra_msat_first_sid_header = header[68:72]
-	#Get Total number of sectors used for the master sector allocation table (MSAT).
-	total_sectors_msat_header = header[72:76]
-	#Get First part of the master sector allocation table (MSAT) containing 109 sector identifiers (SIDs).
-	og_msat = header[76:512]
+		elif(endian_header == little_endian):
+			endian = "<"
+
+		#Get sector size
+		sector_size_header = header[30:32]
+		short_sector_size_header = header[32:34]
+		#get sector allocation table size
+		sat_size_header = header[44:48]
+		#get directory first sector SID
+		directory_first_sector_sid_header = header[48:52]
+		#get min size of standard stream
+		min_stream_size_header = header[56:60]
+		#get Sector identifier (SID) of first sector of the short-sector allocation table (SSAT).
+		short_first_sector_sid_header = header[60:64]
+		#Get Total number of sectors used for the short-sector allocation table (SSAT).
+		total_sectors_ssat_header = header[64:68]
+		#Get Sector identifier (SID) of first sector of the extra master sector allocation table (MSAT)
+		extra_msat_first_sid_header = header[68:72]
+		#Get Total number of sectors used for the master sector allocation table (MSAT).
+		total_sectors_msat_header = header[72:76]
+		#Get First part of the master sector allocation table (MSAT) containing 109 sector identifiers (SIDs).
+		og_msat = header[76:512]
 
 
-	sector_size = 2**unpack_bytes("h", sector_size_header)
-	short_sector_size = 2**unpack_bytes("h", short_sector_size_header)
+		sector_size = 2**unpack_bytes("h", sector_size_header)
+		short_sector_size = 2**unpack_bytes("h", short_sector_size_header)
 
-	#Extract msat
-	msat_arr = extract_og_msat(og_msat)
+		#Extract msat
+		msat_arr = extract_og_msat(og_msat)
 
-	#Get SAT
-	sat_byte_arr = get_sat(msat_arr, sector_size, rest_of_file)
+		#Get SAT
+		sat_byte_arr = get_sat(msat_arr, sector_size, rest_of_file)
 
-	#Convert SAT byte arr to decimal
-	sat_arr = extract_sat_arr(sat_byte_arr)
-	#Convert ssat size byte arr to decimal
-	total_sectors_ssat = unpack_bytes("i", total_sectors_ssat_header)
+		#Convert SAT byte arr to decimal
+		sat_arr = extract_sat_arr(sat_byte_arr)
+		#Convert ssat size byte arr to decimal
+		total_sectors_ssat = unpack_bytes("i", total_sectors_ssat_header)
 
-	#Get decimal of first SSAT value
-	short_first_sector_sid = unpack_bytes("i", short_first_sector_sid_header)
+		#Get decimal of first SSAT value
+		short_first_sector_sid = unpack_bytes("i", short_first_sector_sid_header)
 
-	#Get SSAT 
-	if(short_first_sector_sid > 0):
-		ssat_arr = get_ssat(short_first_sector_sid, sector_size, total_sectors_ssat, rest_of_file)
+		#Get SSAT 
+		if(short_first_sector_sid > 0):
+			ssat_arr = get_ssat(short_first_sector_sid, sector_size, total_sectors_ssat, rest_of_file)
 
-	#Convert directory start sid to decimal
-	directory_first_sector_sid = unpack_bytes("i", directory_first_sector_sid_header)
+		#Convert directory start sid to decimal
+		directory_first_sector_sid = unpack_bytes("i", directory_first_sector_sid_header)
 
-	#Get Directory bytes
-	directory_bytes = get_directory_bytes(directory_first_sector_sid, sat_arr, sector_size, whole_file)
+		#Get Directory bytes
+		directory_bytes = get_directory_bytes(directory_first_sector_sid, sat_arr, sector_size, whole_file)
 
-	#Parse directory
-	dir_dict = parse_directory(directory_bytes)
+		#Parse directory
+		dir_dict = parse_directory(directory_bytes)
 
-	#Extract root dir
-	root_entry_bytes = dir_dict[root_entry_str]
+		#Extract root dir
+		root_entry_bytes = dir_dict[root_entry_str]
 
-	#Extract dest list entry
-	dest_list_entry_bytes = dir_dict[dir_dest_list_str]
+		#Extract dest list entry
+		dest_list_entry_bytes = dir_dict[dir_dest_list_str]
 
-	#Convert min stream size bytes
-	min_stream_size = unpack_bytes("i", min_stream_size_header)
+		#Convert min stream size bytes
+		min_stream_size = unpack_bytes("i", min_stream_size_header)
 
-	#Extract dest list from bytes
-	dest_list_entry_dict = extract_dir_entry(dest_list_entry_bytes)
+		#Extract dest list from bytes
+		dest_list_entry_dict = extract_dir_entry(dest_list_entry_bytes)
 
-	#Extract root from bytes
-	root_entry_dict = extract_dir_entry(root_entry_bytes)
+		#Extract root from bytes
+		root_entry_dict = extract_dir_entry(root_entry_bytes)
 
-	#Get Root data
-	root_data = b''
-	if(check_size(min_stream_size, root_entry_dict["size"])):
-		root_data = get_data_run(root_entry_dict["sid"], sat_arr, sector_size, whole_file)
-	else:
-		root_data = get_data_run(root_entry_dict["sid"], ssat_arr, sector_size, whole_file)
-	#Get Dirlist data
-	dir_list_full_data = b''
-	if(check_size(min_stream_size, dest_list_entry_dict["size"])):
-		dest_list_full_data = get_data_run(dest_list_entry_dict["sid"], sat_arr, sector_size, whole_file)
-	else:
-		dest_list_full_data = get_data_run(dest_list_entry_dict["sid"], ssat_arr, short_sector_size, whole_file, root_data)
+		#Get Root data
+		root_data = b''
+		if(check_size(min_stream_size, root_entry_dict["size"])):
+			root_data = get_data_run(root_entry_dict["sid"], sat_arr, sector_size, whole_file)
+		else:
+			root_data = get_data_run(root_entry_dict["sid"], ssat_arr, sector_size, whole_file)
+		#Get Dirlist data
+		dir_list_full_data = b''
+		if(check_size(min_stream_size, dest_list_entry_dict["size"])):
+			dest_list_full_data = get_data_run(dest_list_entry_dict["sid"], sat_arr, sector_size, whole_file)
+		else:
+			dest_list_full_data = get_data_run(dest_list_entry_dict["sid"], ssat_arr, short_sector_size, whole_file, root_data)
 
-	#Parse Dirlist data
-	entry_remain = True
-	start_point = 0
-	version = unpack_bytes("i", dest_list_full_data[0:4])
-	dest_list_data = dest_list_full_data[dest_list_header_size:]
-	dest_list_entries = []
-	while(entry_remain):
-		entry_remain = False
-		new_dest_list_entry = extract_destlist_entry(dest_list_data, version, start_point)
-		dest_list_entries.append(new_dest_list_entry)
-		if(version == version_one):
-			start_point += version_one_entry_path_offset + new_dest_list_entry["path_length"]
-			length_rest = len(dest_list_data[start_point:])
-			if(length_rest > version_one_entry_path_offset):
-				entry_remain = True
-		elif(version == version_three):
-			start_point += version_three_entry_path_offset + new_dest_list_entry["path_length"]
-			length_rest = len(dest_list_data[start_point:])
-			if(length_rest > version_three_entry_path_offset):
-				entry_remain = True
-	#Loop through destlist entries
-	for destlist_entry in dest_list_entries:
-		if(destlist_entry["entry_id"] != 0):
-			entry = extract_dir_entry(dir_dict[str(destlist_entry["entry_id"])])
-			if(check_size(min_stream_size, entry["size"])):
-				entry_data = get_data_run(entry["sid"], sat_arr, sector_size, whole_file)
-			else:
-				entry_data = get_data_run(entry["sid"], ssat_arr, short_sector_size, whole_file, root_data)
-			lnk_dict = parse_lnk(entry_data)
-			lnk_dict["creation"] = convert_timestamp(lnk_dict["creation"]).strftime("%m/%d/%Y %H:%M:%S")
-			lnk_dict["last_access"] = convert_timestamp(lnk_dict["last_access"]).strftime("%m/%d/%Y %H:%M:%S")
-			lnk_dict["last_modify"] = convert_timestamp(lnk_dict["last_modify"]).strftime("%m/%d/%Y %H:%M:%S")
-			print("Path: " + destlist_entry["path"])
-			print("Lnk Creation: " + lnk_dict["creation"])
-			print("Lnk Last Access: " + lnk_dict["last_access"])
-			print("Lnk Last Modify: " + lnk_dict["last_modify"])
-			print()
+		#Parse Dirlist data
+		entry_remain = True
+		start_point = 0
+		version = unpack_bytes("i", dest_list_full_data[0:4])
+		dest_list_data = dest_list_full_data[dest_list_header_size:]
+		dest_list_entries = []
+		while(entry_remain):
+			entry_remain = False
+			new_dest_list_entry = extract_destlist_entry(dest_list_data, version, start_point)
+			dest_list_entries.append(new_dest_list_entry)
+			if(version == version_one):
+				start_point += version_one_entry_path_offset + new_dest_list_entry["path_length"]
+				length_rest = len(dest_list_data[start_point:])
+				if(length_rest > version_one_entry_path_offset):
+					entry_remain = True
+			elif(version == version_three):
+				start_point += version_three_entry_path_offset + new_dest_list_entry["path_length"]
+				length_rest = len(dest_list_data[start_point:])
+				if(length_rest > version_three_entry_path_offset):
+					entry_remain = True
+		#Loop through destlist entries
+		for destlist_entry in dest_list_entries:
+			if(destlist_entry["entry_id"] != 0):
+				entry = extract_dir_entry(dir_dict[str(destlist_entry["entry_id"])])
+				if(check_size(min_stream_size, entry["size"])):
+					entry_data = get_data_run(entry["sid"], sat_arr, sector_size, whole_file)
+				else:
+					entry_data = get_data_run(entry["sid"], ssat_arr, short_sector_size, whole_file, root_data)
+				lnk_dict = parse_lnk(entry_data)
+				lnk_dict["creation"] = convert_timestamp(lnk_dict["creation"]).strftime("%m/%d/%Y %H:%M:%S")
+				lnk_dict["last_access"] = convert_timestamp(lnk_dict["last_access"]).strftime("%m/%d/%Y %H:%M:%S")
+				lnk_dict["last_modify"] = convert_timestamp(lnk_dict["last_modify"]).strftime("%m/%d/%Y %H:%M:%S")
+				print("Path: " + destlist_entry["path"])
+				print("Lnk Creation: " + lnk_dict["creation"])
+				print("Lnk Last Access: " + lnk_dict["last_access"])
+				print("Lnk Last Modify: " + lnk_dict["last_modify"])
+				print()
+	elif(jle_type == "c"):
+		file_sig = header[0:36]
+		if(file_sig != custom_header):
+			print("The specified file is not a custom jumplist")
+			exit()
+		lnk_exists = True
+		lnk_offset = 0
+		jle_file.seek(0)
+		lnk_bytes = jle_file.read()
+		lnk_already_read = 0
+		for i in range(0,2):
+			lnk_offset = lnk_bytes.find(lnk_guid)
+			print(lnk_offset)
+			if(lnk_offset == -1):
+				lnk_exists = False
+				break
+			lnk_already_read += lnk_offset
+			jle_file.seek(lnk_offset)
+			lnk_bytes = jle_file.read()
+			lnk_dict = parse_lnk(lnk_bytes)
+			print(lnk_dict)
+			jle_file.seek(lnk_offset + lnk_guid_size)
+			print(lnk_offset + lnk_guid_size)
+			lnk_bytes = jle_file.read()
+			# f = open("test", "wb")
+			# f.write(lnk_bytes)
+			# f.close()
+			# lnk_dict["creation"] = convert_timestamp(lnk_dict["creation"]).strftime("%m/%d/%Y %H:%M:%S")
+			# lnk_dict["last_access"] = convert_timestamp(lnk_dict["last_access"]).strftime("%m/%d/%Y %H:%M:%S")
+			# lnk_dict["last_modify"] = convert_timestamp(lnk_dict["last_modify"]).strftime("%m/%d/%Y %H:%M:%S")
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
